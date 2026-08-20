3ad3d8fc2ed44bea8de87a23147a855fb2649055
SanthoshT12345 <santhoshthirumalai70@gmail.com>
2026-08-20 17:54:21 +0530

    frontend Configuration

--- progress-tracker-frontend/src/api/axiosInstance.js
+++ progress-tracker-frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://task-tracker-2p3s.onrender.com",
 });
 
 axiosInstance.interceptors.request.use((config) => {
